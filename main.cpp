0bac5895cdc27f12af4ffaa689e6e095649295d7
Adam <AB-HardWorkBeatsNaturalTalent@users.noreply.github.com>
2022-02-15 19:51:13 +0000

    spaces

--- main.cpp
+++ main.cpp
@@ -144,7 +144,7 @@ int main()
     auto* smaller = f.compare( &t1, &t2 );                              //8
     if(smaller != nullptr)
     {
-        std::cout << "the smaller one is << " << smaller->name <<             std::endl; //9    
+        std::cout << "the smaller one is << " << smaller->name << std::endl; //9    
     }
     
     U u3;
